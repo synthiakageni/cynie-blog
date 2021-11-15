6c4389bac567b365036738483823397f540ae1fb
synthiakageni <synthia.kageni@student.moringaschool.com>
2021-11-15 13:08:48 +0300

    update files

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from flask_script import Manager,Server
 from flask_migrate import Migrate, MigrateCommand
 from app.models import User,Role
 # Creating app instance
-app = create_app('development')
+app = create_app('production')
 manager = Manager(app)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
